3beeec0b67da8219a6b5487d56d7db86ebe78aa4
mamamiyear <mamamiyear@163.com>
2018-12-06 23:54:37 +0800

    add feture: build and activate

--- build/build.sh
+++ build/build.sh
@@ -2,11 +2,12 @@
 
 cur_dir=$(cd $(dirname $0); pwd)
 src_dir=$cur_dir/../src
-release_dir=$cur_dir/../release
+output_dir=$cur_dir/../release
+backup_dir=$cur_dir/../backup
 
 cd ${cur_dir}
 
-[ -d ${release_dir} ] || mkdir ${release_dir}
+[ -d ${output_dir} ] || mkdir ${output_dir}
 
 src_file=(vimrc.title vimrc.plugin vimrc.basic)
 
@@ -16,12 +17,19 @@ for vimrc in ${src_file[@]}
 do
     cat ${src_dir}/${vimrc} >> vimrc.temp
     echo "build: ${vimrc}"
-#ls ${src_dir}/ | grep vimrc_ | xargs -I {} cat ${src_dir}/{} >> vimrc.temp
 done
 
 time=$(date "+%Y%m%d%H%M%S")
-cp vimrc.temp ${release_dir}/vimrc_${time}
-echo "output: ${release_dir}/vimrc_${time}"
+cp vimrc.temp ${output_dir}/vimrc_${time}
+echo "output: ${output_dir}/vimrc_${time}"
+
+
+if [ -n "$1" -a "$1" = "activate" ]; then
+    [ -d ${backup_dir} ] || mkdir ${backup_dir}
+    cp ${HOME}/.vimrc ${backup_dir}/vimrc_${time}_backup
+    cat vimrc.temp > ${HOME}/.vimrc
+    echo "Activate new vimrc_${time} successfully. Original ${HOME}/.vimrc has been backup in ${backup_dir}/vimrc_${time}_backup"
+fi
 
 rm vimrc.temp >> /dev/null
 
